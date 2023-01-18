b7731f35ab6df5236b31d28a1378ecc64e09aa08
gioleon <giovannileon2017@gmail.com>
2023-01-18 18:23:57 -0500

    creation of projects backend using spring Initialzr and frontend angular CLI

---
+++ signup-login-backend/src/main/java/com/signuploginbackend/SignupLoginBackendApplication.java
@@ -0,0 +1,13 @@
+package com.signuploginbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupLoginBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupLoginBackendApplication.class, args);
+	}
+
+}
---
+++ signup-login-backend/src/test/java/com/signuploginbackend/SignupLoginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signuploginbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupLoginBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
